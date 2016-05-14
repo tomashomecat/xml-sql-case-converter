f5823243efe451445779db67d8a2fec3b7ff4e32
tomashomecat <tomashomecat@msn.com>
2016-05-14 10:46:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace xml_case_convert
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.button2 = new System.Windows.Forms.Button();
+      this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button3 = new System.Windows.Forms.Button();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.listBox2 = new System.Windows.Forms.ListBox();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.tableLayoutPanel2.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Location = new System.Drawing.Point(272, 3);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(263, 476);
+      this.listBox1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button1.Location = new System.Drawing.Point(3, 485);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(263, 24);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "folder";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 2;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Controls.Add(this.listBox2, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.listBox1, 1, 0);
+      this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+      this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 2;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(538, 512);
+      this.tableLayoutPanel1.TabIndex = 3;
+      // 
+      // button2
+      // 
+      this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button2.Location = new System.Drawing.Point(137, 3);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(61, 24);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "C > c";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // tableLayoutPanel2
+      // 
+      this.tableLayoutPanel2.ColumnCount = 3;
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+      this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 0);
+      this.tableLayoutPanel2.Controls.Add(this.button3, 2, 0);
+      this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel2.Location = new System.Drawing.Point(269, 482);
+      this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+      this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+      this.tableLayoutPanel2.RowCount = 1;
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel2.Size = new System.Drawing.Size(269, 30);
+      this.tableLayoutPanel2.TabIndex = 3;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.textBox1.Location = new System.Drawing.Point(0, 5);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(0, 5, 0, 0);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(134, 20);
+      this.textBox1.TabIndex = 3;
+      this.textBox1.Text = "Text";
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(204, 3);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(62, 23);
+      this.button3.TabIndex = 4;
+      this.button3.Text = "c > C";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      this.openFileDialog1.Multiselect = true;
+      this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+      // 
+      // listBox2
+      // 
+      this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.listBox2.FormattingEnabled = true;
+      this.listBox2.Location = new System.Drawing.Point(3, 3);
+      this.listBox2.Name = "listBox2";
+      this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+      this.listBox2.Size = new System.Drawing.Size(263, 476);
+      this.listBox2.TabIndex = 4;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(538, 512);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel2.ResumeLayout(false);
+      this.tableLayoutPanel2.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.ListBox listBox2;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace xml_case_convert
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      this.openFileDialog1.ShowDialog();
+    }
+
+    private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+    {
+      Match m;
+      this.Activate();
+      this.listBox2.Items.Clear();
+      m = Regex.Match(openFileDialog1.FileName, @".*[\\]");
+      this.listBox2.Tag = m.Value; 
+      foreach (string fn in openFileDialog1.FileNames)
+      {
+        m = Regex.Match(fn, @"[^\\]*$");
+        this.listBox2.Items.Add(m.Value); 
+      }
+      this.listBox2.Refresh();
+    }
+
+    private void processXml(string btn)
+    {
+      XmlDocument xml;
+      XmlNodeList nds;
+      foreach (string fn in listBox2.SelectedItems)
+      {
+        xml = new XmlDocument();
+        xml.Load(listBox2.Tag + fn);
+        nds = xml.GetElementsByTagName(this.textBox1.Text);
+        foreach (XmlNode nd in nds)
+        {
+          if (btn == "C > c")
+          {
+            nd.InnerText = nd.InnerText.ToLower();
+            // check if there exists N'... unicode string
+            nd.InnerText = Regex.Replace(nd.InnerText, @"n'", @"N'");
+          }
+          else if (btn == "c > C")
+            nd.InnerText = nd.InnerText.ToUpper();
+        }
+        if (!System.IO.Directory.Exists(listBox2.Tag + "xcn")) System.IO.Directory.CreateDirectory(listBox2.Tag + "xcn");
+        xml.Save(listBox2.Tag + "xcn\\" + fn);
+        this.listBox1.Items.Add(fn);
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      // upper to lower inside all elements with defined name
+      processXml(((Button)sender).Text);
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      // lower to upper inside all elements with defined name
+      processXml(((Button)sender).Text);
+    }
+  }
+}
